1c1944456f37c5cf721579a2bfb5eb6e923ba107
swooasd <kyle.swensson@gmail.com>
2015-10-17 02:07:12 -0700

    made base fly boss heart

---
+++ core/src/com/mygdx/game2/FlyBossHeart.java
@@ -0,0 +1,13 @@
+package com.mygdx.game2;
+
+/**
+ * Created by kyle on 10/16/2015.
+ */
+public class FlyBossHeart extends Enemy implements Boss {
+
+    public FlyBossHeart(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+}
